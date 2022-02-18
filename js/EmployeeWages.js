ba8bf13df696721d9f54452e7d1b3276d05cf4ee
vinit <vinitsxn@gmail.com>
2022-02-18 19:40:28 +0530

    Added: On Load Form Functionality Added

--- js/EmployeeWages.js
+++ js/EmployeeWages.js
@@ -92,3 +92,20 @@ salary.addEventListener("input", function () {
   output.textContent = salary.value;
 });
 
+//on Load
+window.addEventListener("DOMContentLoaded", (event) => {
+  const name = document.querySelector("#name");
+  const textError = document.querySelector(".text-error");
+  name.addEventListener("input", function () {
+    if (name.value.length == 0) {
+      textError.textContent = "";
+    } else {
+      try {
+        new EmployeePayrollData().name = name.value;
+        textError.textContent = "";
+      } catch (e) {
+        textError.textContent = e;
+      }
+    }
+  });
+});
