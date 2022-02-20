7461469f2e26c289fa79f6dc991eaa59c45b5c7b
vinit <vinitsxn@gmail.com>
2022-02-20 18:05:54 +0530

    Added: View Employee Patroll Details from JSON Object

--- js/new_home.js
+++ js/new_home.js
@@ -2,45 +2,61 @@ window.addEventListener("DOMContentLoaded", (event) => {
   createInnerHtml();
 });
 const createInnerHtml = () => {
-  const inner = `<tr>
-    <th></th>
-    <th>Name</th>
-    <th>Gender</th>
-    <th>Department</th>
-    <th>Salary</th>
-    <th>Start Date</th>
-    <th>Actions</th>
-  </tr>
-  <tr>
+  const headerHtml = `<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>`;
+  let empPayrollList = createEmployeePayrollJSON();
+  let innerHtml = `${headerHtml}`;
+  for (const empPayrollData of empPayrollList) {
+    innerHtml = `${innerHtml} 
+  <tr> 
     <td>
-      <img
-        class="profile"
-        alt=""
-        src="../assets/profile-images/Ellipse -2.png"
-      />
-    </td>
-    <td>Narayan Mahadevan</td>
-    <td>Male</td>
-    <td>
-      <div class="dept-label">HR</div>
-      <div class="dept-label">Finance</div>
-    </td>
-    <td>3000000</td>
-    <td>1 Nov 2020</td>
-    <td>
-      <img
-        name="l"
-        onclick="remove(this)"
-        alt="delete"
-        src="../assets/icons/delete-black-18dp.svg"
-      />
-      <img
-        name="1"
-        alt="edit"
-        onclick="update(this)"
-        src="../assets/icons/create-black-18dp.svg"
-      />
-    </td>
+      <img class="profile" src="${empPayrollData._profilePic}" alt="">
+    </td> 
+    <td>${empPayrollData._name}</td> 
+    <td>${empPayrollData._gender}</td>
+    <td>${getDeptHtml(empPayrollData._department)}</td> 
+    <td>${empPayrollData._salary}</td> 
+    <td>${empPayrollData._startDate}</td> 
+    <td> 
+      <img name="${
+        empPayrollData._id
+      }" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
+      <img name="${
+        empPayrollData._id
+      }" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">    
+    </td> 
   </tr>`;
-  document.getElementById("display").innerHTML = inner;
+  }
+  document.querySelector("#display").innerHTML = innerHtml;
+};
+const getDeptHtml = (deptList) => {
+  let deptHtml = "";
+  for (const dept of deptList) {
+    deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
+  }
+  return deptHtml;
+};
+const createEmployeePayrollJSON = () => {
+  let empPayrollListLocal = [
+    {
+      _name: "Narayan Mahadevan",
+      _gender: "Male",
+      _department: ["Engineering", "Finance"],
+      _salary: "500000",
+      _startDate: "29 Oct 2019",
+      _note: "",
+      _id: new Date().getTime(),
+      _profilePic: "../assets/profile-images/Ellipse -2.png",
+    },
+    {
+      _name: "Amarpa Shashanka Keerthi Kumar",
+      _gender: "Female",
+      _department: ["Sales"],
+      _salary: "400000",
+      _startDate: "29 Oct 2019",
+      _note: "",
+      _id: new Date().getTime() + 1,
+      _profilePic: "../assets/profile-images/Ellipse -1.png",
+    },
+  ];
+  return empPayrollListLocal;
 };
