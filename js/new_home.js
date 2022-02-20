def7f3c17bdaeb06deb03803463ec8410cbdbbba
vinit <vinitsxn@gmail.com>
2022-02-20 18:57:28 +0530

    Added: View Employee Payroll Data from Local Storage Functionality added

--- js/new_home.js
+++ js/new_home.js
@@ -1,20 +1,31 @@
+let empPayrollList;
 window.addEventListener("DOMContentLoaded", (event) => {
+  empPayrollList = getEmployeeDataFromStorage();
+  document.querySelector(".emp-count").textContent = empPayrollList.length;
   createInnerHtml();
+  localStorage.removeItem("editEmp");
 });
+
+const getEmployeeDataFromStorage = () => {
+  return localStorage.getItem("EmployeePayrollList")
+    ? JSON.parse(localStorage.getItem("EmployeePayrollList"))
+    : [];
+};
+
 const createInnerHtml = () => {
   const headerHtml = `<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>`;
-  let empPayrollList = createEmployeePayrollJSON();
+  if (empPayrollList.length == 0) return;
   let innerHtml = `${headerHtml}`;
   for (const empPayrollData of empPayrollList) {
     innerHtml = `${innerHtml} 
   <tr> 
     <td>
-      <img class="profile" src="${empPayrollData._profilePic}" alt="">
+      <img class="profile" src="../assets/${empPayrollData._profilePic}" alt="">
     </td> 
     <td>${empPayrollData._name}</td> 
     <td>${empPayrollData._gender}</td>
     <td>${getDeptHtml(empPayrollData._department)}</td> 
-    <td>${empPayrollData._salary}</td> 
+    <td>₹ ${empPayrollData._salary}</td> 
     <td>${empPayrollData._startDate}</td> 
     <td> 
       <img name="${
@@ -28,35 +39,11 @@ const createInnerHtml = () => {
   }
   document.querySelector("#display").innerHTML = innerHtml;
 };
+
 const getDeptHtml = (deptList) => {
   let deptHtml = "";
   for (const dept of deptList) {
     deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
   }
   return deptHtml;
-};
-const createEmployeePayrollJSON = () => {
-  let empPayrollListLocal = [
-    {
-      _name: "Narayan Mahadevan",
-      _gender: "Male",
-      _department: ["Engineering", "Finance"],
-      _salary: "500000",
-      _startDate: "29 Oct 2019",
-      _note: "",
-      _id: new Date().getTime(),
-      _profilePic: "../assets/profile-images/Ellipse -2.png",
-    },
-    {
-      _name: "Amarpa Shashanka Keerthi Kumar",
-      _gender: "Female",
-      _department: ["Sales"],
-      _salary: "400000",
-      _startDate: "29 Oct 2019",
-      _note: "",
-      _id: new Date().getTime() + 1,
-      _profilePic: "../assets/profile-images/Ellipse -1.png",
-    },
-  ];
-  return empPayrollListLocal;
 };
